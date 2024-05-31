26e9ea8592f8e4a7d52faeee2c4a805a92ff9ba9
ShMPt <cacahouettetbs@gmail.com>
2024-05-31 10:28:31 +0200

    feat: addition of delete button on shopping list

--- pages/JSDOMApps/script/toDoList.js
+++ pages/JSDOMApps/script/toDoList.js
@@ -12,20 +12,45 @@ function getShoppingListItem() {
 
 };
 
-function addNewItem() {
 
-    let itemContent = getShoppingListItem();
-    if (itemContent === "") { return; }
-    itemData.push(itemContent);
-
+function refreshList() {
     shoppingList.innerHTML = "";
 
     for (let n in itemData) {
         let newItem = document.createElement('li');
         newItem.innerText = itemData[n];
+
+        createDeleteButton(newItem);
+ 
         shoppingList.append(newItem);
     }
+}
+
+function addNewItem() {
+
+    let itemContent = getShoppingListItem();
+    if (itemContent === "") { return; }
+
+    itemData.push(itemContent);
+
+    refreshList();
 
     document.querySelector('form').reset();
+
+}
+
+function createDeleteButton(newItem) {
+    let deleteButton = document.createElement('button');
+        deleteButton.setAttribute("type", "button");
+        deleteButton.addEventListener("click", function(){
+            let itemToDelete = this.closest("li").innerText.slice(0,-1);
+            index = itemData.indexOf(itemToDelete);
+            itemData.splice(index, 1);
+            refreshList();
+        })
+        deleteButton.classList.add("deleter");
+        deleteButton.innerText = "x";
+
+        newItem.append(deleteButton);
 
 }
